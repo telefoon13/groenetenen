a4a8f10e2bb6cf39e60e21356de4e0d6719bbfbf
Mike Dhoore <mike@mikedhoore.be>
2017-11-28 15:55:14 +0100

    Hoofdstuk 27

--- src/main/java/be/vdab/repositories/RepositoriesConfig.java
+++ src/main/java/be/vdab/repositories/RepositoriesConfig.java
@@ -1,9 +1,24 @@
 package be.vdab.repositories;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan
 public class RepositoriesConfig {
+
+	@Bean
+	JdbcTemplate jdbcTemplate(DataSource dataSource){
+		return new JdbcTemplate(dataSource);
+	}
+
+	@Bean
+	NamedParameterJdbcTemplate namedParameterJdbcTemplate (DataSource dataSource){
+		return new NamedParameterJdbcTemplate(dataSource);
+	}
 }
